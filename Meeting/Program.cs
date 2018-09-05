1a3f5766390f4bf2a0496239e5004478c747d9b1
koenmetsu <koen@kometsoft.be>
2018-09-05 17:54:08 +0200

    Initial commit

---
+++ Meeting/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Diconvergent.Meeting
+{  
+    public class Program
+    {
+        private ProgramSlot[] _program;
+        
+        public Program(ProgramSlot[] program)
+        {
+            _program = program;
+        }
+    }
+}
